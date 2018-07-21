f5cf4b8f6722101d98a8755ba86a0d72b0c02c2f
pseudophpt <pseudophpt@gmail.com>
2018-07-21 18:16:20 -0400

    Update bootcode to pseultra specifications

--- boot/boot.s
+++ boot/boot.s
@@ -2,7 +2,7 @@
 # pseultra/boot/boot.s
 # Bootcode
 # 
-# (C) pseudophpt 2018
+# (C) pseudophpt, Nabile Rahmani 2018
 #
 
 # N64 Definitions
@@ -11,26 +11,29 @@
 # Code begins at 0x40
 .org 0x40
 
+# First stage in RSP DMEM
+.Stage1Start:
+
 # Initialize COP0 registers
 mtc0 $zero, Cause # Clear interrupt cause
 mtc0 $zero, Count # Clear cycle count
 mtc0 $zero, Compare # Clear cycle timer compare
 
-# DMA first megabyte of ROM to 0x400
-li $t1, 0x10000000 | KSEG1
-lw $t0, 8($t1) # Load the boot address from the cartridge header.
-li $t2, 0xFFFFFF
-and $t0, $t0, $t2 # Mask the DRAM address to 24-bit, as per the DMA spec.
+# DMA boot segment to 0x1000
+li $t4, CART_DOM1_ADDR2 | KSEG1
+lw $t3, 0x10($t4) # Start of boot segment in ROM
+lw $t2, 0x14($t4) # Length of boot segment
+addiu $t2, -1
+
 li $t1, PI_DRAM_ADDR_REG | KSEG1
-sw $t0, 0($t1)
+ori $t0, $zero, 0x1000
+sw $t0, 0($t1) # Load at 0x1000
 
 li $t1, PI_CART_ADDR_REG | KSEG1
-li $t0, 0x10001000 # 0x1000 in ROM
-sw $t0, 0($t1)
+sw $t3, 0($t1)
 
 li $t1, PI_WR_LEN_REG | KSEG1
-li $t0, 0xFFFFF # One megabyte
-sw $t0, 0($t1)
+sw $t2, 0($t1)
 
 # Unmask RCP interrupts
 li $t1, MI_INTR_MASK_REG | KSEG1
@@ -67,7 +70,30 @@ sw $s3, 0x04($t0)
 sw $s5, 0x0c($t0)
 sw $s7, 0x14($t0)
 
-# Jump to ROM
-li $t0, 0x10000000 | KSEG1
-lw $t0, 8($t0)
+# Copy second stage to RDRAM at 0x300
+li $t0, .Stage2Start # Source
+li $t1, 0x400 | KSEG0 # Destination
+li $t2, .Stage2End # End
+
+.LoadStage2Word:
+lw $t3, 0($t0) # From source
+sw $t3, 0($t1) # To destination
+addiu $t0, 0x04 # Increment locations
+addiu $t1, 0x04
+bne $t0, $t2, .LoadStage2Word # If not yet at the end, copy another word
+
+li $t0, 0x400 | KSEG0
 jr $t0
+
+.Stage1End:
+
+.Stage2Start:
+
+# Jump to boot segment
+li $t0, CART_DOM1_ADDR2 | KSEG0
+lw $t0, 8($t0) # Relative offset
+li $t1, KSEG0 | 0x1000
+addu $t0, $t1 # Add KSEG0
+jr $t0
+
+.Stage2End:
