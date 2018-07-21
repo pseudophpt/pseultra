dd8ada3c0374815c88a443e801b13f5791bdfa77
pseudophpt <pseudophpt@gmail.com>
2018-07-21 08:44:26 -0400

    Fix operands

--- boot/boot.s
+++ boot/boot.s
@@ -17,22 +17,22 @@ mtc0 $zero, Count # Clear cycle count
 mtc0 $zero, Compare # Clear cycle timer compare
 
 # DMA first megabyte of ROM to 0x400
-li $t0, PI_DRAM_ADDR_REG | KSEG1
-ori $t1, 0x400
+li $t1, PI_DRAM_ADDR_REG | KSEG1
+ori $t0, $zero, 0x400
 sw $t0, 0($t1)
 
-li $t0, PI_CART_ADDR_REG | KSEG1
-ori $t1, 0x1000
+li $t1, PI_CART_ADDR_REG | KSEG1
+ori $t0, $zero, 0x1000
 sw $t0, 0($t1)
 
-li $t0, PI_RD_LEN_REG | KSEG1
-li $t1, 0xFFFFFF
+li $t1, PI_RD_LEN_REG | KSEG1
+li $t0, 0xFFFFFF
 sw $t0, 0($t1)
 
 # Unmask RCP interrupts
-li $t0, MI_INTR_MASK_REG | KSEG1
-ori $t1, 0x555
-sw $t1, 0($t0)
+li $t1, MI_INTR_MASK_REG | KSEG1
+ori $t0, $zero, 0x555
+sw $t0, 0($t1)
 
 # Clear each RCP interrupt cause bit
 li $t0, AI_STATUS_REG | KSEG1
