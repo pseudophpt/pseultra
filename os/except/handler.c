24080126fcf93d54a013724bcd56771e8a126f20
pseudophpt <pseudophpt@gmail.com>
2018-07-25 05:49:22 -0400

    Remove debug variable

--- os/except/handler.c
+++ os/except/handler.c
@@ -12,8 +12,6 @@ extern u32 __osHandlerEnd;
 
 void __osInitExceptions (void) {
     // Install exception handler for the three non-NMI exceptions
-    int x = __osHandlerStart;
-
     osCopyMemory((void *)INT_VEC_TLB_MISS, (void *)&__osHandlerStart, (u32)&__osHandlerEnd - (u32)&__osHandlerStart);
     osCopyMemory((void *)INT_VEC_XTLB_MISS, (void *)&__osHandlerStart, (u32)&__osHandlerEnd - (u32)&__osHandlerStart);
     osCopyMemory((void *)INT_VEC_GENERAL, (void *)&__osHandlerStart, (u32)&__osHandlerEnd - (u32)&__osHandlerStart);
