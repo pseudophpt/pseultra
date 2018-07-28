adc9e64beb4f74db835f8eb7ae325b9194642deb
pseudophpt <pseudophpt@gmail.com>
2018-07-28 03:11:07 -0400

    Fix loadreg macro to load registers instead of save again

--- os/event/exception_s.s
+++ os/event/exception_s.s
@@ -31,7 +31,7 @@ lui $k0, %hi(__osExceptionRegSave)
 addiu $k0, %lo(__osExceptionRegSave)
 
 .irp reg,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31
-sw $\reg, (\reg * 4)($k0)
+lw $\reg, (\reg * 4)($k0)
 .endr
 
 .endm
