71b6d045a6f525fb677f4d93211ff118b1a06c33
pseudophpt <pseudophpt@gmail.com>
2018-08-21 00:44:13 -0400

    Add COP0 registers to ucode rsp.h

--- n64/ucode/include/ucode/rsp.h
+++ n64/ucode/include/ucode/rsp.h
@@ -18,11 +18,33 @@
 #define UCODE_RSP_H_GUARD
 
 /*
- * RSP specific instruction definitions
+ * RSP COP0 Registers
  */
 #ifdef __asm__
 
+#define DMA_CACHE $0
+#define DMA_DRAM $1
+#define DMA_READ_LENGTH $2
+#define DMA_WRITE_LENGTH $3
+#define SP_STATUS $4
+#define DMA_FULL $5
+#define DMA_BUSY $6
+#define SP_RESERVED $7
+#define CMD_START $8
+#define CMD_END $9
+#define CMD_CURRENT $10
+#define CMD_STATUS $11
+#define CMD_CLOCK $12
+#define CMD_BUSY $13
+#define CMD_PIPE_BUSY $14
+#define CMD_TMEM_BUSY $15
 
+#endif
+
+/*
+ * RSP specific instruction definitions
+ */
+#ifdef __asm__
 
 #endif
 
