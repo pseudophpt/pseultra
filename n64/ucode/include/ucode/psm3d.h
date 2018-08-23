ddbbf49707b4e40b0b6b7ff872dfdc330d72f2d2
pseudophpt <pseudophpt@gmail.com>
2018-08-23 13:41:41 -0400

    Add macros for Set_Combine_Mode and Set_Convert

--- n64/ucode/include/ucode/psm3d.h
+++ n64/ucode/include/ucode/psm3d.h
@@ -41,7 +41,6 @@ typedef struct __attribute__((packed, aligned(8))) uPSM3DDispCmd_t {
 #define UCODE_PSM3D_OP_END_DL 0x01
 
 #define UCODE_PSM3D_OP_SET_BLEND_MODE 0x05
-#define UCODE_PSM3D_OP_SET_COMBINE_MODE 0x06
 
 /*
  * Operation macros
@@ -163,6 +162,112 @@ typedef struct __attribute__((packed, aligned(8))) uPSM3DDispCmd_t {
         ((k2 & 0x1f) << 27) | (k3 << 18) | (k4 << 9) | k5,\
         (k0 << 13) | (k1 << 4) \
     )
+
+#define UCODE_PSM3D_CC_A_COMB 0
+#define UCODE_PSM3D_CC_A_TEX0 1
+#define UCODE_PSM3D_CC_A_TEX1 2
+#define UCODE_PSM3D_CC_A_PRIM 3
+#define UCODE_PSM3D_CC_A_SHADE 4
+#define UCODE_PSM3D_CC_A_ENV 5
+#define UCODE_PSM3D_CC_A_ONE 6
+#define UCODE_PSM3D_CC_A_NOISE 7
+#define UCODE_PSM3D_CC_A_ZERO 8
+
+#define UCODE_PSM3D_CC_B_COMB 0
+#define UCODE_PSM3D_CC_B_TEX0 1
+#define UCODE_PSM3D_CC_B_TEX1 2
+#define UCODE_PSM3D_CC_B_PRIM 3
+#define UCODE_PSM3D_CC_B_SHADE 4
+#define UCODE_PSM3D_CC_B_ENV 5
+#define UCODE_PSM3D_CC_B_KEYC 6
+#define UCODE_PSM3D_CC_B_K4 7
+#define UCODE_PSM3D_CC_B_ZERO 8
+
+#define UCODE_PSM3D_CC_C_COMB 0
+#define UCODE_PSM3D_CC_C_TEX0 1
+#define UCODE_PSM3D_CC_C_TEX1 2
+#define UCODE_PSM3D_CC_C_PRIM 3
+#define UCODE_PSM3D_CC_C_SHADE 4
+#define UCODE_PSM3D_CC_C_ENV 5
+#define UCODE_PSM3D_CC_C_KEYS 6
+#define UCODE_PSM3D_CC_C_COMBA 7
+#define UCODE_PSM3D_CC_C_TEX0A 8
+#define UCODE_PSM3D_CC_C_TEX1A 9
+#define UCODE_PSM3D_CC_C_PRIMA 10
+#define UCODE_PSM3D_CC_C_SHADEA 11
+#define UCODE_PSM3D_CC_C_ENVA 12
+#define UCODE_PSM3D_CC_C_LOD 13
+#define UCODE_PSM3D_CC_C_PRLOD 14
+#define UCODE_PSM3D_CC_C_K5 15
+#define UCODE_PSM3D_CC_C_ZERO 16
+
+#define UCODE_PSM3D_CC_D_COMB 0
+#define UCODE_PSM3D_CC_D_TEX0 1
+#define UCODE_PSM3D_CC_D_TEX1 2
+#define UCODE_PSM3D_CC_D_PRIM 3
+#define UCODE_PSM3D_CC_D_SHADE 4
+#define UCODE_PSM3D_CC_D_ENV 5
+#define UCODE_PSM3D_CC_D_ONE 6
+#define UCODE_PSM3D_CC_D_ZERO 7
+
+#define UCODE_PSM3D_AC_A_COMB 0
+#define UCODE_PSM3D_AC_A_TEX0 1
+#define UCODE_PSM3D_AC_A_TEX1 2
+#define UCODE_PSM3D_AC_A_PRIM 3
+#define UCODE_PSM3D_AC_A_SHADE 4
+#define UCODE_PSM3D_AC_A_ENV 5
+#define UCODE_PSM3D_AC_A_ONE 6
+#define UCODE_PSM3D_AC_A_ZERO 7
+
+#define UCODE_PSM3D_AC_B_COMB 0
+#define UCODE_PSM3D_AC_B_TEX0 1
+#define UCODE_PSM3D_AC_B_TEX1 2
+#define UCODE_PSM3D_AC_B_PRIM 3
+#define UCODE_PSM3D_AC_B_SHADE 4
+#define UCODE_PSM3D_AC_B_ENV 5
+#define UCODE_PSM3D_AC_B_ONE 6
+#define UCODE_PSM3D_AC_B_ZERO 7
+
+#define UCODE_PSM3D_AC_C_LOD 0
+#define UCODE_PSM3D_AC_C_TEX0 1
+#define UCODE_PSM3D_AC_C_TEX1 2
+#define UCODE_PSM3D_AC_C_PRIM 3
+#define UCODE_PSM3D_AC_C_SHADE 4
+#define UCODE_PSM3D_AC_C_ENV 5
+#define UCODE_PSM3D_AC_C_PRLOD 6
+#define UCODE_PSM3D_AC_C_ZERO 7
+
+#define UCODE_PSM3D_AC_D_COMB 0
+#define UCODE_PSM3D_AC_D_TEX0 1
+#define UCODE_PSM3D_AC_D_TEX1 2
+#define UCODE_PSM3D_AC_D_PRIM 3
+#define UCODE_PSM3D_AC_D_SHADE 4
+#define UCODE_PSM3D_AC_D_ENV 5
+#define UCODE_PSM3D_AC_D_ONE 6
+#define UCODE_PSM3D_AC_D_ZERO 7
+
+#define usPSM3DSetCombineMode(cca0, ccb0, ccc0, ccd0, aca0, acb0, acc0, acd0, cca1, ccb1, ccc1, ccd1, aca1, acb1, acc1, acd1) \
+    usPSM3DRdpThrough(\
+        UCODE_RDP_OPC_Set_Combine_Mode,\
+        (UCODE_PSM3D_CC_A_##cca0 << 20) |\
+        (UCODE_PSM3D_CC_C_##ccc0 << 15) |\
+        (UCODE_PSM3D_AC_A_##aca0 << 12) |\
+        (UCODE_PSM3D_AC_C_##acc0 << 9) |\
+        (UCODE_PSM3D_CC_A_##cca1 << 5) |\
+        (UCODE_PSM3D_CC_C_##ccc1),\
+            \
+        (UCODE_PSM3D_CC_B_##ccb0 << 28) |\
+        (UCODE_PSM3D_CC_B_##ccb1 << 24) |\
+        (UCODE_PSM3D_AC_A_##aca1 << 21) |\
+        (UCODE_PSM3D_AC_C_##acc1 << 18) |\
+        (UCODE_PSM3D_CC_D_##ccd0 << 15) |\
+        (UCODE_PSM3D_AC_B_##acb0 << 12) |\
+        (UCODE_PSM3D_AC_D_##acd0 << 9) |\
+        (UCODE_PSM3D_CC_D_##ccd1 << 6) |\
+        (UCODE_PSM3D_AC_B_##acb1 << 3) |\
+        (UCODE_PSM3D_AC_D_##acd1)\
+    )
+
 
 /*
  * Microcode locations
