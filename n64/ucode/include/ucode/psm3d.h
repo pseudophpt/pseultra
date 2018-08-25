285247adb883baadeac5be58b0f38d2263944803
pseudophpt <pseudophpt@gmail.com>
2018-08-25 04:03:21 -0400

    Add static microcode macros

--- n64/ucode/include/ucode/psm3d.h
+++ n64/ucode/include/ucode/psm3d.h
@@ -46,10 +46,13 @@ typedef struct __attribute__((packed, aligned(8))) uPSM3DDispCmd_t {
  */
 
 #define usPSM3DNoop() { (UCODE_PSM3D_OP_NOOP << 24), 0 } 
+#define uPSM3DNoop(dl) *((dl) ++) = (uPSM3DDispCmd) { (UCODE_PSM3D_OP_NOOP << 24), 0 } 
 
 #define usPSM3DEndDL() { (UCODE_PSM3D_OP_END_DL << 24), 0 }
+#define uPSM3DEndDL(dl) *((dl) ++) = (uPSM3DDispCmd) { (UCODE_PSM3D_OP_END_DL << 24), 0 }
 
 #define usPSM3DSetOtherMode(maskh, maskl, datah, datal) { (UCODE_PSM3D_OP_SET_OTHER_MODE << 24) | maskh , datah } , { maskl , datal }
+#define uPSM3DSetOtherMode(dl, maskh, maskl, datah, datal) *((dl)++) = (uPSM3DDispCmd) { (UCODE_PSM3D_OP_SET_OTHER_MODE << 24) | maskh , datah }; *((dl) ++) = (uPSM3DDispCmd) { maskl , datal }
 
 #define UCODE_PSM3D_BLEND_MODE_M1A_IN 0
 #define UCODE_PSM3D_BLEND_MODE_M1A_MEM 1
@@ -79,7 +82,16 @@ typedef struct __attribute__((packed, aligned(8))) uPSM3DDispCmd_t {
         (UCODE_PSM3D_BLEND_MODE_M1B_##m1b0 << 26) | (UCODE_PSM3D_BLEND_MODE_M1B_##m1b1 << 24) |\
         (UCODE_PSM3D_BLEND_MODE_M2A_##m2a0 << 22) | (UCODE_PSM3D_BLEND_MODE_M2A_##m2a1 << 20) |\
         (UCODE_PSM3D_BLEND_MODE_M2B_##m2b0 << 18) | (UCODE_PSM3D_BLEND_MODE_M2B_##m2b1) \
-    )\
+    )
+#define uPSM3DSetBlendMode(dl, m1a0, m2a0, m1b0, m2b0, m1a1, m2a1, m1b1, m2b1) \
+    uPSM3DSetOtherMode(dl, \
+        0x0000, 0xFFFF0000,\
+        0,\
+        (UCODE_PSM3D_BLEND_MODE_M1A_##m1a0 << 30) | (UCODE_PSM3D_BLEND_MODE_M1A_##m1a1 << 28) |\
+        (UCODE_PSM3D_BLEND_MODE_M1B_##m1b0 << 26) | (UCODE_PSM3D_BLEND_MODE_M1B_##m1b1 << 24) |\
+        (UCODE_PSM3D_BLEND_MODE_M2A_##m2a0 << 22) | (UCODE_PSM3D_BLEND_MODE_M2A_##m2a1 << 20) |\
+        (UCODE_PSM3D_BLEND_MODE_M2B_##m2b0 << 18) | (UCODE_PSM3D_BLEND_MODE_M2B_##m2b1) \
+    )
 
 
 #define UCODE_PSM3D_SET_IMAGE_FMT_RGBA 0
@@ -101,6 +113,14 @@ typedef struct __attribute__((packed, aligned(8))) uPSM3DDispCmd_t {
             \
         data2\
     }
+#define uPSM3DRdpThrough(dl, opcode, data1, data2) \
+    *((dl) ++) = (uPSM3DDispCmd) {\
+        (UCODE_PSM3D_OP_RDP_THROUGH << 24) |\
+        (opcode << 24) |\
+        data1,\
+            \
+        data2\
+    }
 
 #define usPSM3DSetColorImg(dram_addr, format, size, width) \
     usPSM3DRdpThrough(\
@@ -111,6 +131,16 @@ typedef struct __attribute__((packed, aligned(8))) uPSM3DDispCmd_t {
             \
         dram_addr\
     )
+#define uPSM3DSetColorImg(dl, dram_addr, format, size, width) \
+    uPSM3DRdpThrough(dl, \
+        UCODE_RDP_OPC_Set_Color_Image,\
+        (UCODE_PSM3D_SET_IMAGE_FMT_##format << 21) |\
+        (UCODE_PSM3D_SET_IMAGE_SIZE_##size << 19) |\
+        (width - 1), \
+            \
+        dram_addr\
+    )
+
 
 #define usPSM3DSetTextureImg(dram_addr, format, size, width) \
     usPSM3DRdpThrough(\
@@ -121,6 +151,107 @@ typedef struct __attribute__((packed, aligned(8))) uPSM3DDispCmd_t {
             \
         dram_addr\
     )
+#define uPSM3DSetTextureImg(dl, dram_addr, format, size, width) \
+    uPSM3DRdpThrough(dl, \
+        UCODE_RDP_OPC_Set_Texture_Image,\
+        (UCODE_PSM3D_SET_IMAGE_FMT_##format << 21) |\
+        (UCODE_PSM3D_SET_IMAGE_SIZE_##size << 19) |\
+        (width - 1), \
+            \
+        dram_addr\
+    )
+
+#define UCODE_PSM3D_SET_TILE_FMT_RGBA 0
+#define UCODE_PSM3D_SET_TILE_FMT_YUV 1
+#define UCODE_PSM3D_SET_TILE_FMT_CI 2
+#define UCODE_PSM3D_SET_TILE_FMT_IA 3
+#define UCODE_PSM3D_SET_TILE_FMT_I 4
+
+#define UCODE_PSM3D_SET_TILE_SIZE_4BPP 0
+#define UCODE_PSM3D_SET_TILE_SIZE_8BPP 1
+#define UCODE_PSM3D_SET_TILE_SIZE_16BPP 2
+#define UCODE_PSM3D_SET_TILE_SIZE_32BPP 3
+
+#define usPSM3DSetTile(format, csize, size, tmem_addr, tile, palette, ct, mt, cs, ms, maskt, shiftt, masks, shifts) \
+    usPSM3DRdpThrough(\
+        UCODE_RDP_OPC_Set_Tile,\
+        (UCODE_PSM3D_SET_TILE_FMT_##format << 21) |\
+        (UCODE_PSM3D_SET_TILE_SIZE_##csize << 19) |\
+        (size << 9) |\
+        (tmem_addr),\
+            \
+        (tile << 24) |\
+        (palette << 20) |\
+        (ct << 19) |\
+        (mt << 18) |\
+        (maskt << 14) |\
+        (shiftt << 10) |\
+        (cs << 9) |\
+        (ms << 8) |\
+        (masks << 4) |\
+        (shifts) |\
+    )
+#define uPSM3DSetTile(dl, format, csize, size, tmem_addr, tile, palette, ct, mt, cs, ms, maskt, shiftt, masks, shifts) \
+    uPSM3DRdpThrough(dl, \
+        UCODE_RDP_OPC_Set_Tile,\
+        (UCODE_PSM3D_SET_TILE_FMT_##format << 21) |\
+        (UCODE_PSM3D_SET_TILE_SIZE_##csize << 19) |\
+        (size << 9) |\
+        (tmem_addr),\
+            \
+        (tile << 24) |\
+        (palette << 20) |\
+        (ct << 19) |\
+        (mt << 18) |\
+        (maskt << 14) |\
+        (shiftt << 10) |\
+        (cs << 9) |\
+        (ms << 8) |\
+        (masks << 4) |\
+        (shifts)\
+    )
+
+#define usPSM3DLoadTile(tile, sh, th, sl, tl) \
+    usPSM3DRdpThrough(\
+        UCODE_RDP_OPC_Load_Tile,\
+        (sl << 12) |\
+        (tl), \
+            \
+        (tile << 24) |\
+        (sh << 12) |\
+        (th)\
+    )
+#define uPSM3DLoadTile(dl, tile, sh, th, sl, tl) \
+    uPSM3DRdpThrough(dl, \
+        UCODE_RDP_OPC_Load_Tile,\
+        (sl << 12) |\
+        (tl), \
+            \
+        (tile << 24) |\
+        (sh << 12) |\
+        (th)\
+    )
+
+#define usPSM3DSetTileSize(tile, sh, th, sl, tl) \
+    usPSM3DRdpThrough(\
+        UCODE_RDP_OPC_Set_Tile_Size,\
+        (sl << 12) |\
+        (tl), \
+            \
+        (tile << 24) |\
+        (sh << 12) |\
+        (th)\
+    )
+#define uPSM3DSetTileSize(dl, tile, sh, th, sl, tl) \
+    uPSM3DRdpThrough(dl, \
+        UCODE_RDP_OPC_Set_Tile_Size,\
+        (sl << 12) |\
+        (tl), \
+            \
+        (tile << 24) |\
+        (sh << 12) |\
+        (th)\
+    )
 
 #define usPSM3DSetEnvColor(r, g, b, a) \
     usPSM3DRdpThrough(\
@@ -128,9 +259,22 @@ typedef struct __attribute__((packed, aligned(8))) uPSM3DDispCmd_t {
         0,\
         (r << 24) | (g << 16) | (b << 8) | a\
     )
+#define uPSM3DSetEnvColor(dl, r, g, b, a) \
+    uPSM3DRdpThrough(dl, \
+        UCODE_RDP_OPC_Set_Env_Color,\
+        0,\
+        (r << 24) | (g << 16) | (b << 8) | a\
+    )
+
 
 #define usPSM3DSetBlendColor(r, g, b, a) \
     usPSM3DRdpThrough(\
+        UCODE_RDP_OPC_Set_Blend_Color,\
+        0,\
+        (r << 24) | (g << 16) | (b << 8) | a\
+    )
+#define uPSM3DSetBlendColor(dl, r, g, b, a) \
+    uPSM3DRdpThrough(dl, \
         UCODE_RDP_OPC_Set_Blend_Color,\
         0,\
         (r << 24) | (g << 16) | (b << 8) | a\
@@ -142,9 +286,21 @@ typedef struct __attribute__((packed, aligned(8))) uPSM3DDispCmd_t {
         0,\
         (r << 24) | (g << 16) | (b << 8) | a\
     )
+#define uPSM3DSetFogColor(dl, r, g, b, a) \
+    uPSM3DRdpThrough(dl, \
+        UCODE_RDP_OPC_Set_Fog_Color,\
+        0,\
+        (r << 24) | (g << 16) | (b << 8) | a\
+    )
 
 #define usPSM3DSetFillColor(color) \
     usPSM3DRdpThrough(\
+        UCODE_RDP_OPC_Set_Fill_Color,\
+        0,\
+        color\
+    )
+#define uPSM3DSetFillColor(dl, color) \
+    uPSM3DRdpThrough(dl, \
         UCODE_RDP_OPC_Set_Fill_Color,\
         0,\
         color\
@@ -156,9 +312,21 @@ typedef struct __attribute__((packed, aligned(8))) uPSM3DDispCmd_t {
         0,\
         (z << 16) | (delta << 8)\
     )
+#define uPSM3DSetPrimDepth(dl, z, delta) \
+    uPSM3DRdpThrough(dl, \
+        UCODE_RDP_OPC_Set_Prim_Depth,\
+        0,\
+        (z << 16) | (delta << 8)\
+    )
 
 #define usPSM3DSyncFull() \
     usPSM3DRdpThrough(\
+        UCODE_RDP_OPC_Sync_Full,\
+        0,\
+        0\
+    )
+#define uPSM3DSyncFull(dl) \
+    uPSM3DRdpThrough(dl, \
         UCODE_RDP_OPC_Sync_Full,\
         0,\
         0\
@@ -170,9 +338,21 @@ typedef struct __attribute__((packed, aligned(8))) uPSM3DDispCmd_t {
         0,\
         0\
     )
+#define uPSM3DSyncPipe(dl) \
+    uPSM3DRdpThrough(\
+        UCODE_RDP_OPC_Sync_Pipe,\
+        0,\
+        0\
+    )
 
 #define usPSM3DSyncLoad() \
     usPSM3DRdpThrough(\
+        UCODE_RDP_OPC_Sync_Load,\
+        0,\
+        0\
+    )
+#define uPSM3DSyncLoad(dl) \
+    uPSM3DRdpThrough(dl, \
         UCODE_RDP_OPC_Sync_Load,\
         0,\
         0\
@@ -186,6 +366,12 @@ typedef struct __attribute__((packed, aligned(8))) uPSM3DDispCmd_t {
         (xh << 12) | yh,\
         (xl << 12) | yl\
     )
+#define uPSM3DSetScissor(dl, xh, yh, xl, yl) \
+    uPSM3DRdpThrough(dl, \
+        UCODE_RDP_OPC_Set_Scissor,\
+        (xh << 12) | yh,\
+        (xl << 12) | yl\
+    )
 
 #define usPSM3DSetConvert(k0, k1, k2, k3, k4, k5) \
     usPSM3DRdpThrough(\
@@ -193,9 +379,21 @@ typedef struct __attribute__((packed, aligned(8))) uPSM3DDispCmd_t {
         ((k2 & 0x1f) << 27) | (k3 << 18) | (k4 << 9) | k5,\
         (k0 << 13) | (k1 << 4) \
     )
+#define uPSM3DSetConvert(dl, k0, k1, k2, k3, k4, k5) \
+    uPSM3DRdpThrough(dl, \
+        UCODE_RDP_OPC_Set_Convert,\
+        ((k2 & 0x1f) << 27) | (k3 << 18) | (k4 << 9) | k5,\
+        (k0 << 13) | (k1 << 4) \
+    )
 
 #define usPSM3DFillRect(xh, yh, xl, yl) \
     usPSM3DRdpThrough(\
+        UCODE_RDP_OPC_Fill_Rectangle,\
+        (xl << 12) | yl,\
+        (xh << 12) | yh\
+    )
+#define uPSM3DFillRect(dl, xh, yh, xl, yl) \
+    uPSM3DRdpThrough(dl, \
         UCODE_RDP_OPC_Fill_Rectangle,\
         (xl << 12) | yl,\
         (xh << 12) | yh\
@@ -305,6 +503,29 @@ typedef struct __attribute__((packed, aligned(8))) uPSM3DDispCmd_t {
         (UCODE_PSM3D_AC_B_##acb1 << 3) |\
         (UCODE_PSM3D_AC_D_##acd1)\
     )
+
+#define uPSM3DSetCombineMode(dl, cca0, ccb0, ccc0, ccd0, aca0, acb0, acc0, acd0, cca1, ccb1, ccc1, ccd1, aca1, acb1, acc1, acd1) \
+    uPSM3DRdpThrough(dl, \
+        UCODE_RDP_OPC_Set_Combine_Mode,\
+        (UCODE_PSM3D_CC_A_##cca0 << 20) |\
+        (UCODE_PSM3D_CC_C_##ccc0 << 15) |\
+        (UCODE_PSM3D_AC_A_##aca0 << 12) |\
+        (UCODE_PSM3D_AC_C_##acc0 << 9) |\
+        (UCODE_PSM3D_CC_A_##cca1 << 5) |\
+        (UCODE_PSM3D_CC_C_##ccc1),\
+            \
+        (UCODE_PSM3D_CC_B_##ccb0 << 28) |\
+        (UCODE_PSM3D_CC_B_##ccb1 << 24) |\
+        (UCODE_PSM3D_AC_A_##aca1 << 21) |\
+        (UCODE_PSM3D_AC_C_##acc1 << 18) |\
+        (UCODE_PSM3D_CC_D_##ccd0 << 15) |\
+        (UCODE_PSM3D_AC_B_##acb0 << 12) |\
+        (UCODE_PSM3D_AC_D_##acd0 << 9) |\
+        (UCODE_PSM3D_CC_D_##ccd1 << 6) |\
+        (UCODE_PSM3D_AC_B_##acb1 << 3) |\
+        (UCODE_PSM3D_AC_D_##acd1)\
+    )
+
 
 
 /*
