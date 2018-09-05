dbefb9fb70e652ce79ffaaa820a8efbba28babdc
pseudophpt <pseudophpt@gmail.com>
2018-09-05 00:05:12 -0400

    Add scalar register definitions

--- n64/ucode/include/ucode/rsp.h
+++ n64/ucode/include/ucode/rsp.h
@@ -142,6 +142,75 @@ undefinst teqi
 undefinst tnei
 
 /*
+ * Replace scalar registers
+ */
+#undef zero
+#undef at
+#undef v0
+#undef v1
+#undef a0
+#undef a1
+#undef a2
+#undef a3
+#undef t0
+#undef t1
+#undef t2
+#undef t3
+#undef t4
+#undef t5
+#undef t6
+#undef t7
+#undef s0
+#undef s1
+#undef s2
+#undef s3
+#undef s4
+#undef s5
+#undef s6
+#undef s7
+#undef t8
+#undef t9
+#undef k0
+#undef k1
+#undef gp
+#undef sp
+#undef fp
+#undef ra
+
+#define r0 $0
+#define r1 $1
+#define r2 $2
+#define r3 $3
+#define r4 $4
+#define r5 $5
+#define r6 $6
+#define r7 $7
+#define r8 $8
+#define r9 $9
+#define r10 $10
+#define r11 $11
+#define r12 $12
+#define r13 $13
+#define r14 $14
+#define r15 $15
+#define r16 $16
+#define r17 $17
+#define r18 $18
+#define r19 $19
+#define r20 $20
+#define r21 $21
+#define r22 $22
+#define r23 $23
+#define r24 $24
+#define r25 $25
+#define r26 $26
+#define r27 $27
+#define r28 $28
+#define r29 $29
+#define r30 $30
+#define r31 $31
+
+/*
  * Element registers
  */
 #define f 0
